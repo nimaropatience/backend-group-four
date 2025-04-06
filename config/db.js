45fee041b65a0186e06c6c30a66e384190720490
maria-kisakye <kismar1120@gmail.com>
2025-04-06 16:24:56 +0300

    config

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
+
+module.exports = db;
